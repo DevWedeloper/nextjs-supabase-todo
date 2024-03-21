e0f8915f69f602e7537cc6aa85b6b800fd7be264
DevWedeloper <vicnathangabrielle@gmail.com>
2024-03-22 01:52:13 +0800

    feat: add sorting

--- src/app/todo/columns.tsx
+++ src/app/todo/columns.tsx
@@ -1,20 +1,52 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
 import { Todo } from '@/lib/types';
 import { ColumnDef } from '@tanstack/react-table';
+import { ArrowUpDown } from 'lucide-react';
 
 export const columns: ColumnDef<Todo>[] = [
   {
     accessorKey: 'task',
-    header: 'Task',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Task
+          <ArrowUpDown className='ml-2 h-4 w-4' />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: 'created_at',
-    header: 'Created At',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Created At
+          <ArrowUpDown className='ml-2 h-4 w-4' />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: 'due_date',
-    header: 'Due Date',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Due Date
+          <ArrowUpDown className='ml-2 h-4 w-4' />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: 'completed',
