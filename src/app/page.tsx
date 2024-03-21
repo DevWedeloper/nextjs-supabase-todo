3be77fb8cc6521c97cdc1f361db99355a8613e33
DevWedeloper <vicnathangabrielle@gmail.com>
2024-03-22 01:54:30 +0800

    refactor: add more dummy data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,69 @@ async function getData(): Promise<Todo[]> {
       due_date: new Date('2024-03-28'),
       completed: true,
     },
+    {
+      id: 3,
+      created_at: new Date(),
+      task: 'Mock Task 3',
+      due_date: new Date('2024-03-30'),
+      completed: false,
+    },
+    {
+      id: 4,
+      created_at: new Date(),
+      task: 'Mock Task 4',
+      due_date: new Date('2024-04-02'),
+      completed: true,
+    },
+    {
+      id: 5,
+      created_at: new Date(),
+      task: 'Mock Task 5',
+      due_date: new Date('2024-04-05'),
+      completed: false,
+    },
+    {
+      id: 6,
+      created_at: new Date(),
+      task: 'Mock Task 6',
+      due_date: new Date('2024-04-08'),
+      completed: false,
+    },
+    {
+      id: 7,
+      created_at: new Date(),
+      task: 'Mock Task 7',
+      due_date: new Date('2024-04-10'),
+      completed: false,
+    },
+    {
+      id: 8,
+      created_at: new Date(),
+      task: 'Mock Task 8',
+      due_date: new Date('2024-04-15'),
+      completed: true,
+    },
+    {
+      id: 9,
+      created_at: new Date(),
+      task: 'Mock Task 9',
+      due_date: new Date('2024-04-18'),
+      completed: false,
+    },
+    {
+      id: 10,
+      created_at: new Date(),
+      task: 'Mock Task 10',
+      due_date: new Date('2024-04-20'),
+      completed: false,
+    },
+    {
+      id: 11,
+      created_at: new Date(),
+      task: 'Mock Task 11',
+      due_date: new Date('2024-04-22'),
+      completed: false,
+    }
   ];
 }
 
