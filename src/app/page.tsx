028ea9640b1a84b6473af73c094a4555d586484f
DevWedeloper <vicnathangabrielle@gmail.com>
2024-03-26 02:30:07 +0800

    feat: access searchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,15 @@ import AddTodo from './add-todo';
 import { columns } from './todo/columns';
 import { DataTable } from './todo/data-table';
 
-export default async function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
   const supabase = createClient();
   const { data } = await supabase.from('todo').select().order('task');
-
+  console.log(searchParams);
+  
   return (
     <div className='flex min-h-screen items-center justify-center p-4'>
       <div className='flex w-full flex-col gap-4'>
