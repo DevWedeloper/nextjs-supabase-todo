c8b4b986552d55a5dca2a46acf8c40fc3cd50d72
DevWedeloper <vicnathangabrielle@gmail.com>
2024-03-26 23:38:29 +0800

    feat: add supabase pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,26 @@ export default async function Home({
 }: {
   searchParams?: { [key: string]: string | string[] | undefined };
 }) {
+  const page = parseInt((searchParams?.page as string) ?? '1', 10);
+  const pageSize = parseInt((searchParams?.pageSize as string) ?? '10', 10);
+  const sortBy = (searchParams?.sortBy as string) ?? 'task';
+  const sortOrder = (searchParams?.sortOrder as string) ?? 'asc';
+  const searchQuery = (searchParams?.searchQuery as string) ?? '';
+  const offset = (page - 1) * pageSize;
+
   const supabase = createClient();
-  const { data } = await supabase.from('todo').select().order('task');
-  console.log(searchParams);
+
+  let query = supabase.from('todo').select('*', { count: 'exact' });
+  if (searchQuery) {
+    query = query.filter('task', 'ilike', `%${searchQuery}%`);
+  }
+
+  const { data, count } = await query
+    .order(sortBy, { ascending: sortOrder === 'asc' })
+    .range(offset, offset + pageSize - 1);
   
+  console.log(count);
+
   return (
     <div className='flex min-h-screen items-center justify-center p-4'>
       <div className='flex w-full flex-col gap-4'>
