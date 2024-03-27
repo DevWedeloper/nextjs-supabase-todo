e958ef8735aa4ebb18d851fa5ebed0bcec19ddb4
DevWedeloper <vicnathangabrielle@gmail.com>
2024-03-27 19:40:13 +0800

    feat: add manage selection button

--- src/app/todo/selected-todos-info.tsx
+++ src/app/todo/selected-todos-info.tsx
@@ -1,13 +1,25 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
 import { useSelectedTodosStore } from '@/store/selected-todos';
 
 export default function SelectedTodosInfo({ count }: { count: number }) {
   const { selectedTodoIds } = useSelectedTodosStore();
 
   return (
-    <div className='text-muted-foreground flex-1 text-sm'>
-      {selectedTodoIds.length} of {count} row(s) selected.
+    <div className='flex flex-1 items-center gap-16'>
+      <div className='text-muted-foreground text-sm'>
+        {selectedTodoIds.length} of {count} row(s) selected.
+      </div>
+      {selectedTodoIds.length > 0 && (
+        <Button
+          variant='ghost'
+          size='sm'
+          className='text-sm text-blue-500 hover:!text-blue-500'
+        >
+          Manage selection
+        </Button>
+      )}
     </div>
   );
 }
