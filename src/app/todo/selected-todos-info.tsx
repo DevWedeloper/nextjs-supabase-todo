ac6b67b59d98f58fdc4f4956ad06eb52f3cd36ce
DevWedeloper <vicnathangabrielle@gmail.com>
2024-03-27 19:46:46 +0800

    feat: add dropdown

--- src/app/todo/selected-todos-info.tsx
+++ src/app/todo/selected-todos-info.tsx
@@ -1,10 +1,23 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import { useSelectedTodosStore } from '@/store/selected-todos';
+import { Trash2 } from 'lucide-react';
 
 export default function SelectedTodosInfo({ count }: { count: number }) {
   const { selectedTodoIds } = useSelectedTodosStore();
+
+  const handleOnClick = () => {
+    console.log('clicked');
+  };
 
   return (
     <div className='flex flex-1 items-center gap-16'>
@@ -12,13 +25,25 @@ export default function SelectedTodosInfo({ count }: { count: number }) {
         {selectedTodoIds.length} of {count} row(s) selected.
       </div>
       {selectedTodoIds.length > 0 && (
-        <Button
-          variant='ghost'
-          size='sm'
-          className='text-sm text-blue-500 hover:!text-blue-500'
-        >
-          Manage selection
-        </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant='ghost'
+              size='sm'
+              className='text-sm text-blue-500 hover:!text-blue-500'
+            >
+              Manage selection
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end'>
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={handleOnClick}>
+              <Trash2 className='mr-2 h-4 w-4 text-red-500' />
+              <span>Delete All</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       )}
     </div>
   );
